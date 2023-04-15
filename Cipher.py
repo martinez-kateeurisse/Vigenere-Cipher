2523014863dbfb4852296f8b4989e123f5ead223
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-04-16 01:25:09 +0800

    Pseudocode

---
+++ Cipher.py
@@ -0,0 +1,26 @@
+#Kate Eurisse L. Martinez_BSCPE 1-5_OOP Assignment 2-Problem 2
+
+#PROBLEM 3 - The Vigenère Cipher  
+#A program that asks the user for the plaintext and the keyword and produce the ciphertext using the Vigenère cipher. 
+
+#Create a dictionary for the keys and values
+#Initialize lists
+#Ask the user for the message
+    #Iterate over the user's input 
+    #Get the values of each character in the dictionary
+    #Append the value to a list
+#Print the list of values
+
+#Ask the user for the key
+    #Iterate over the user's input 
+    #Get the values of each character in the dictionary
+    #Append the value to a list
+#Print the list of values
+
+#Repeat the values of the key until it matches the lenght of the message
+#Add the values of the message and key and get their mod 26
+#Convert the keys into their corresponding values in the dictionary
+
+#Print the cipher text as a string
+
+#Display the input with the use of pygame
