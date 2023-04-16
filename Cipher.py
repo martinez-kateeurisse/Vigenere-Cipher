9de264713f029df748f12b2935197564773c9223
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-04-16 14:59:47 +0800

    Greetings-added

--- Cipher.py
+++ Cipher.py
@@ -19,6 +19,12 @@ print(Fore.LIGHTYELLOW_EX, """
               ░░▀▄▀░ ▀▀▀ ▀▀▀▀ ▀▀▀ ▀░░▀ ▀▀▀ ▀░▀▀ ▀▀▀ ▒█▄▄█ ▀▀▀ █▀▀▀ ▀░░▀ ▀▀▀ ▀░▀▀ """)                                
 print(Fore.WHITE, " "*12 , "="* 29 + "PROBLEM 3" + "="* 29 ,"\n")
 
+#Asking the user's name and printing a greeting
+print("//" * 20)
+name = input(f"{Fore.RED} Enter your name: "+ Fore.RESET)
+print("//"*20, "\n\n") 
+print(Back.LIGHTBLACK_EX, Fore.LIGHTCYAN_EX, ("Hello " + name).center(91, "*") + Back.RESET, "\n")
+
 #Create a dictionary for the keys and values
 letter_values = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
         'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
